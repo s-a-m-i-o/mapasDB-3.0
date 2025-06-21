e9a58e89a6ae0f46d7ed3cff161c74f8b1643bb6
Samio <s.a.m.i.o@hotmail.com>
2025-06-21 13:40:25 -0300

    Fazendo testes de avisos nas paginas

---
+++ injetar-aviso.js
@@ -0,0 +1,9 @@
+
+fetch('aviso.html')
+    .then(res => res.text())
+    .then(html => {
+        document.getElementById('aviso-container').innerHTML = html;
+    })
+    .catch(err => {
+        console.error('Erro ao carregar aviso de eventos:', err);
+    });
